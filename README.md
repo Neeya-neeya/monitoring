bedd00f023e19052e71ec3b68262f428ef1df7c2
Neeya <sakina.pro04@gmail.com>
2025-12-11 11:58:42 +0100

    Ajout de la feature de surveillance CPU et update du README.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # System Monitor
 
 Mini script Bash de surveillance CPU/RAM/Disque.
+
+# Fonctionnalités 
+
+*Monitoring CPU*
+surveillance de l'utilisation du CPU par les processus en cours.
---
+++ monitor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# --- Surveillance CPU ---
+cpu_usage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
+
+echo "Utilisation CPU : ${cpu_usage}%"
