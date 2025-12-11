ad0027740e98cf74fe154011495567627384b50e
Neeya <sakina.pro04@gmail.com>
2025-12-11 12:16:10 +0100

    Identification et correction du nom de variable RAM : totl --> total

--- monitor.sh
+++ monitor.sh
@@ -6,7 +6,7 @@ cpu_usage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {pr
 echo "Utilisation CPU : ${cpu_usage}%"
 
 #  Surveillance RAM (avec erreur volontaire)
-totl=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+total=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 available=$(grep MemAvailable /proc/meminfo | awk '{print $2}')
 usage=$(echo "scale=2; (1 - $available/$total)*100" | bc) 
 
